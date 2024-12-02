4e9a198483eeaf97890c40ab0dd85ba3257a57f6
Lucas Barros <luc.barros@duocuc.cl>
2024-12-02 18:30:01 -0300

    pruebas en main lista

--- src/app/pages/main/main.page.spec.ts
+++ src/app/pages/main/main.page.spec.ts
@@ -1,11 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainPage } from './main.page';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilidadesService } from 'src/app/services/utilidades.service';
+
+// Mock Services
+class MockFirebaseService {
+  signOut = jasmine.createSpy('signOut');
+}
+
+class MockUtilidadesService {}
 
 describe('MainPage', () => {
   let component: MainPage;
   let fixture: ComponentFixture<MainPage>;
+  let mockFirebaseService: MockFirebaseService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    mockFirebaseService = new MockFirebaseService();
+
+    await TestBed.configureTestingModule({
+      declarations: [MainPage],
+      providers: [
+        { provide: FirebaseService, useValue: mockFirebaseService },
+        { provide: UtilidadesService, useClass: MockUtilidadesService },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MainPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -14,4 +34,10 @@ describe('MainPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call signOut on the FirebaseService when signOut is called', () => {
+    component.signOut();
+    expect(mockFirebaseService.signOut).toHaveBeenCalled();
+  });
 });
+
