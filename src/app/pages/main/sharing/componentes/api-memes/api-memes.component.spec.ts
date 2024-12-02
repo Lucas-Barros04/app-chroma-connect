6b88d131c193db35b026bde8fe1695269ee3c6c9
Lucas Barros <luc.barros@duocuc.cl>
2024-12-02 20:24:01 -0300

    obtener api al 100% echa con otros valores

--- src/app/pages/main/sharing/componentes/api-memes/api-memes.component.spec.ts
+++ src/app/pages/main/sharing/componentes/api-memes/api-memes.component.spec.ts
@@ -1,24 +1,70 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApiMemesComponent } from './api-memes.component';
+import { ApiServiceService } from '../../../api-service.service';
+import { of } from 'rxjs';
+
+// Mock Service
+class MockApiServiceService {
+  obtenerMemes = jasmine.createSpy('obtenerMemes').and.returnValue(of({
+    data: {
+      memes: [
+        { id: 1, name: 'Meme 1' },
+        { id: 2, name: 'Meme 2' }
+      ]
+    }
+  }));
+}
 
 describe('ApiMemesComponent', () => {
   let component: ApiMemesComponent;
   let fixture: ComponentFixture<ApiMemesComponent>;
+  let apiMeme: MockApiServiceService;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    apiMeme = new MockApiServiceService();
+
+    await TestBed.configureTestingModule({
       declarations: [ ApiMemesComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      providers: [
+        { provide: ApiServiceService, useValue: apiMeme }
+      ]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(ApiMemesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should use localStorage data if memes are available', () => {
+    const mockMemes = JSON.stringify([
+      { id: 1, name: 'Meme 1' },
+      { id: 2, name: 'Meme 2' }
+    ]);
+    localStorage.setItem('memes', mockMemes);
+    
+    component.ngOnInit();
+    
+    expect(component.memes).toEqual(JSON.parse(mockMemes));
+    expect(apiMeme.obtenerMemes).not.toHaveBeenCalled(); // No debe llamar a la API
+  });
+
+  it('should fetch memes from API if not in localStorage', () => {
+    localStorage.removeItem('memes'); // Eliminar memes del localStorage
+    
+    component.ngOnInit();
+    
+    expect(apiMeme.obtenerMemes).toHaveBeenCalled(); // Debe llamar a la API
+    expect(component.memes).toEqual([
+      { id: 1, name: 'Meme 1' },
+      { id: 2, name: 'Meme 2' }
+    ]); // Se espera que los memes se asignen correctamente
+    expect(localStorage.getItem('memes')).toBeTruthy(); // Los memes deben guardarse en localStorage
+  });
+
 });
+
